298db3a3b0cf837e135c54781da8177a2a43fc2b
jelluz@gmail.com <jelluz@gmail.com>
2016-08-20 17:34:50 +0200

    + Gps model

--- gps/models.py
+++ gps/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Gps(models.Model):
+    timestamp = models.TimeField(auto_now=True)
     lat = models.FloatField(verbose_name="lattitude")
     lon = models.FloatField(verbose_name="longitude")
